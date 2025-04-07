0f02e9a743025b4937c5de8171f662b09673e310
LiamSuits <suitsliam@gmail.com>
2025-04-07 11:45:15 -0600

    Added persistent attempts in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import GuessInput from "./components/GuessInput.jsx";
 
 const App = () => {
     const [guess, setGuess] = useState('');
-    const [attempts, setAttempts] = useState([]);
+    const [attempts, setAttempts] = useState(() => {
+        const savedAttempts = localStorage.getItem('attempts');
+        if (savedAttempts === null) {
+            return [];
+        } else {
+            return JSON.parse(savedAttempts);
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('attempts', JSON.stringify(attempts));
+    }, [attempts]);
+
+    const baseballSolution = {
+        name: 'Albert Pujols',
+        age: 28,
+        position: '1st Base',
+        statline: '.357/.462/.653 37HR 44SB',
+        team: 'St. Louis Cardinals'
+    }
 
     return (
         <div className="flex flex-col justify-center items-center mt-20">
