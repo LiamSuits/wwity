6542a3fa28ac3f14f1bc5014638e4ec4a2cd08d8
LiamSuits <suitsliam@gmail.com>
2025-04-19 23:21:31 -0600

    Store the winner in localstorage to reduce loads and database touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import GuessInput from "./components/GuessInput.jsx";
 import Toast from "./components/Toast.jsx";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import {db} from "./config/firestore.js";
-import {QuestionBox} from "./components/QuestionBox.jsx";
 import Guesses from "./components/Guesses.jsx";
 import EndGame from "./components/EndGame.jsx";
 
 const App = () => {
+    const today = new Date();
+    const todayFormatted = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+    // const todayFormatted = '18/4/2025';
+    const lastVisit = localStorage.getItem('lastVisitDate');
+    let newDay = false;
+    if (lastVisit === null || lastVisit !== todayFormatted) {
+        // New day detected, clear storage and reset the game
+        newDay = true;
+        localStorage.clear();
+        localStorage.setItem('lastVisitDate', todayFormatted);
+    }
+
     const getWinnerByGameDate = async (gamedate) => {
         try {
             const winnersRef = collection(db, "winners");
@@ -37,30 +48,6 @@ const App = () => {
         localStorage.setItem('winner', JSON.stringify(winner));
     }, [winner]);
 
-    useEffect(() => {
-        const today = new Date();
-        const todayFormatted = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
-        const lastVisit = localStorage.getItem('lastVisitDate');
-        let newDay = false;
-        if (lastVisit === null || lastVisit !== todayFormatted) {
-            // New day detected, clear storage and reset the game
-            newDay = true;
-            localStorage.clear();
-            localStorage.setItem('lastVisitDate', todayFormatted);
-            setAttemptsExhausted(false);
-            setGameWon(false);
-            setAttempts([]);
-            setQuestionBoxHeight('42px');
-        }
-        const fetchWinner = async () => {
-            const result = await getWinnerByGameDate(todayFormatted);
-            setWinner(result);
-        };
-        if (newDay || winner === null) {
-            fetchWinner();
-        }
-    }, []);
-
     const [attempts, setAttempts] = useState(() => {
         const savedAttempts = localStorage.getItem('attempts');
         if (savedAttempts === null) {
@@ -78,12 +65,12 @@ const App = () => {
     }, [attempts]); // When the attempts array is updated, add the new array to localStorage
 
     const [gameWon, setGameWon] = useState(() => {
-       const savedGameWon = localStorage.getItem('gameWon');
-       if (savedGameWon === null) {
-           return false;
-       } else {
-           return savedGameWon === 'true';
-       }
+        const savedGameWon = localStorage.getItem('gameWon');
+        if (savedGameWon === null) {
+            return false;
+        } else {
+            return savedGameWon === 'true';
+        }
     });
 
     useEffect(() => {
@@ -108,9 +95,39 @@ const App = () => {
         localStorage.setItem('attemptsExhausted', JSON.stringify(attemptsExhausted));
     }, [attemptsExhausted]);
 
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    useEffect(() => {
+        const fetchWinner = async () => {
+            const result = await getWinnerByGameDate(todayFormatted);
+            setWinner(result);
+        };
+
+        const updateHasLoaded = async () => {
+            if (newDay || winner === null) {
+                await fetchWinner();  // Ensure fetch finishes before updating state
+            }
+            setHasLoaded(true);  // Only set hasLoaded to true after async work is done
+        };
+
+        updateHasLoaded();
+    }, []);
+
+    // Store height in localStorage to reuse later
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        if (containerRef.current) {
+            const currentHeight = containerRef.current.scrollHeight;
+            // Save height to localStorage to reuse on further renders
+            localStorage.setItem('height', currentHeight);
+            setQuestionBoxHeight(`${currentHeight}px`);
+        }
+    }, [attempts, setQuestionBoxHeight]);
+
     const [showToast, setShowToast] = useState(false);
 
-    if (winner === null) return <p>Loading...</p>;
+    if (winner === null || !hasLoaded) return <p>Loading...</p>;
 
     return (
         <div className="flex flex-col justify-center items-center mt-20 text-center">
@@ -119,7 +136,14 @@ const App = () => {
             )}
             <div className="font-bold">Who Won It That Year?</div>
             <div className="font-bold mt-5">In {winner.year}:</div>
-            <QuestionBox attempts={attempts} height={questionBoxHeight} setHeight={setQuestionBoxHeight} winner={winner} />
+            <div id="question" className="transition-all duration-500 ease-in-out overflow-hidden border-2 pt-2 pb-2.5 rounded-md mb-5 w-65"
+                 style={{height: questionBoxHeight}} ref={containerRef}>
+                Won the {winner.award}
+                {attempts.length > 0 && (<div>Was {winner.age} years old</div>)}
+                {attempts.length > 1 && (<div>Had {winner.statline}</div>)}
+                {attempts.length > 2 && (<div>Played {winner.position}</div>)}
+                {attempts.length > 3 && (<div>Played for the {winner.team}</div>)}
+            </div>
             <Guesses attempts={attempts} gameWon={gameWon} winner={winner} />
             <div className="mt-5">
                 <EndGame attemptsExhausted={attemptsExhausted} gameWon={gameWon} winner={winner} attempts={attempts}
